d41ef920eca4909e15d56634c5d61eda39c7a06b
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2022-03-29 11:37:42 +0900

    week4 컴프 실습

---
+++ Ex03_02.java
@@ -0,0 +1,15 @@
+
+public class Ex03_02 {
+
+	public static void main(String[] args) {
+		int a;
+		float b;
+		
+		a=123;
+		b=(float)200;
+		
+		System.out.printf("a의 값 ==> %d \n",a);
+		System.out.printf("b의 값 ==> %5.1f \n",b);
+	}
+
+}
