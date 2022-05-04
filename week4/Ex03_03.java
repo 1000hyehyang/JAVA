71154127cb725900823dfb36eea12c7c2128ddd0
1000hyehyang <102294782+1000hyehyang@users.noreply.github.com>
2022-05-05 03:11:19 +0900

    자바 week4 실습

--- week4/Ex03_03.java
+++ week4/Ex03_03.java
@@ -8,15 +8,15 @@ public class Ex03_03 {
 		b=a+100;
 		c=a+b-100;
 		d=a+b+c;
-		System.out.printf("a,b,c,dÃ€Ã‡ Â°Âª ==> %d, %d, %d, %d \n", a, b, c, d);
+		System.out.printf("a,b,c,dÀÇ °ª ==> %d, %d, %d, %d \n", a, b, c, d);
 		
 		a=b=100; 
 		c=d=200;
-		System.out.printf("a,b,c,dÃ€Ã‡ Â°Âª ==> %d, %d, %d, %d \n",a,b,c,d);
+		System.out.printf("a,b,c,dÀÇ °ª ==> %d, %d, %d, %d \n",a,b,c,d);
 		
 		a=100;
 		a=a+200;
-		System.out.printf("aÃ€Ã‡ Â°Âª ==> %d \n", a);
+		System.out.printf("aÀÇ °ª ==> %d \n", a);
 		
 
 	}
